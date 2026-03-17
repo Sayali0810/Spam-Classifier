1ff9ce6d8f1f0563e32974cc1f774165b3ac3057
Sayali0810 <116205333+Sayali0810@users.noreply.github.com>
2026-03-18 01:20:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ import streamlit as st
 import pickle
 
 # Load model and vectorizer
-model = pickle.load(open('model.pkl', 'rb'))
-vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
+BASE_DIR = os.path.dirname(__file__)
+model_path = os.path.join(BASE_DIR, "model.pkl")
+vectorizer_path = os.path.join(BASE_DIR, "vectorizer.pkl")
+
+model = pickle.load(open(model_path, "rb"))
+vectorizer = pickle.load(open(vectorizer_path, "rb"))
 
 # Page config
 st.set_page_config(
